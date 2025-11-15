cbc0dfc0e3fff5da8b561edaf16f2400b5d38c30
ms0ur <maksimsuslov7142@gmail.com>
2025-11-15 17:48:23 +0300

    add some modules

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: {
     enabled: true,
 
     timeline: {
       enabled: true
     }
-  }
+  },
+
+  modules: ['@pinia/nuxt', '@nuxt/icon']
 })
